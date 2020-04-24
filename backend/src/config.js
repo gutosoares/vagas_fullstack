200849b18e12c374774e8ff39a782e25f8cd2712
Augusto Soares <gutosoares@outlook.com>
2020-04-23 23:00:30 -0300

    Config backend variables

---
+++ backend/src/config.js
@@ -0,0 +1,5 @@
+global.SALT_KEY = "Bj&(WfOZi{&VA0xhwewlrxy70_NU>/|5xh6bM/{nt$O;/`#{^%FNfZz4%R2']nn"
+
+module.exports = {
+  connect: 'mongodb+srv://gutosoares:L8Xu6letF43IIcVG@cluster0-150zh.mongodb.net/test?retryWrites=true&w=majority'
+}
